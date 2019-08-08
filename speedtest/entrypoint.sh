1652923ea6a5b09ced58582a6a1296b1e246586e
missinglink <insomnia@rcpt.at>
2019-08-08 13:25:33 +0200

    commit

--- speedtest/entrypoint.sh
+++ speedtest/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -o xtrace
 
-apt-get update -y && apt-get install ca-certificates speedtest-cli -y
+apt-get update -y && apt-get install ca-certificates speedtest-cli -y >/dev/null 2>&1
 
 speedtest-cli
